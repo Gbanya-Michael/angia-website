b95f9a6c9a677d34706f82ea417d08e936e7e308
Pennymike <gbanyamichael1@gmail.com>
2023-12-27 03:05:53 +1100

    modified contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -8,52 +8,44 @@ export default function ContactForm() {
           <div className="md:w-1/2">
             <div className="flex gap-1">
               <label
-                htmlFor="service-name"
+                htmlFor="first-name"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Service Name
+                First name
               </label>
-              <span
-                className="text-sm leading-6 text-red-500"
-                id="service-name"
+              <span className="text-sm leading-6 text-red-500" id="first-name">
+                *
+              </span>
+            </div>
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Your first name"
+              aria-describedby="first-name"
+            />
+          </div>
+          <div className="md:w-1/2 mt-5 md:mt-0">
+            <div className="flex gap-1">
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
+                Last name
+              </label>
+              <span className="text-sm leading-6 text-red-500" id="last-name">
                 *
               </span>
             </div>
             <div className="mt-2">
               <input
                 type="text"
-                name="service-name"
-                id="service-name"
+                name="last-name"
+                id="last-name"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Ex: Grooming, hand massage, teeth whitening"
-                aria-describedby="email-optional"
-              />
-            </div>
-          </div>
-          <div className="md:w-1/2 mt-5 md:mt-0">
-            <div className="flex gap-1">
-              <label
-                htmlFor="current-amount"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Current Amount
-              </label>
-              <span
-                className="text-sm leading-6 text-red-500"
-                id="current-amount"
-              >
-                *
-              </span>
-            </div>
-            <div className="mt-2">
-              <input
-                type="number"
-                name="current-amount"
-                id="current-amount"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Numbers only. ex: 2000, 100"
-                aria-describedby="current-amount"
+                placeholder="Your last name"
+                aria-describedby="last-name"
               />
             </div>
           </div>
@@ -62,82 +54,141 @@ export default function ContactForm() {
           <div className="md:w-1/2">
             <div className="flex gap-1">
               <label
-                htmlFor="prev-amount"
+                htmlFor="phone-number"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Previous Amount
+                Phone no.
               </label>
               <span className="text-sm leading-6 text-red-500" id="prev-amount">
                 *
               </span>
             </div>
-            <div className="mt-2">
-              <input
-                type="number"
-                name="prev-amount"
-                id="prev-amount"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                placeholder="Numbers only. ex: 2000, 100"
-                aria-describedby="email-optional"
-              />
-            </div>
+
+            <input
+              type="number"
+              name="prev-amount"
+              id="prev-amount"
+              className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Numbers only. ex: 2000, 100"
+              aria-describedby="email-optional"
+            />
           </div>
-          <div className="md:w-1/2 mt-5 md:mt-0 flex gap-5"></div>
-        </div>
-        <div className="md:flex gap-10 mt-5 ">
-          <div className="md:w-2/3 mt-5 md:mt-0">
+          <div className="md:w-1/2 mt-5 md:mt-0">
             <div className="flex gap-1">
               <label
-                htmlFor="description"
+                htmlFor="email"
                 className="block text-sm font-medium leading-6 text-gray-900"
               >
-                Description
+                Email
               </label>
-              <span className="text-sm leading-6 text-red-500" id="description">
-                *
-              </span>
-            </div>
-            <div className="mt-2">
-              <textarea
-                type="description"
-                name="description"
-                id="description"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
-                placeholder="Max characters: 100 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nunc scelerisque viverra"
-                aria-describedby="description"
-                maxLength={100}
-              ></textarea>
-            </div>
-          </div>
-          <div className="md:w-1/3">
-            <div className="flex gap-1">
-              <label
-                htmlFor="image"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Upload Display Image
-              </label>
-              <span className="text-sm leading-6 text-red-500" id="image">
+              <span className="text-sm leading-6 text-red-500" id="last-name">
                 *
               </span>
             </div>
 
-            <div className="mt-2 flex justify-center rounded-md  ring-1 ring-inset items-center content-center  ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Your email"
+              aria-describedby="email"
+            />
+          </div>
+        </div>
+        <div className="md:flex gap-10 mt-5 ">
+          <div className="md:w-1/2 mt-5 md:mt-0">
+            <div className="mb-5">
+              <div className="flex gap-1">
+                <label
+                  htmlFor="subject"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Subject
+                </label>
+                <span className="text-sm leading-6 text-red-500" id="subject">
+                  *
+                </span>
+              </div>
               <input
-                type="file"
-                accept="image/*"
-                name="image"
-                id="image"
-                className="block w-full h-full border-0 py-2.5 px-5 text-gray-900 shadow-sm   placeholder:text-gray-400  sm:text-sm sm:leading-6"
-                placeholder="Ex: Grooming, hand massage, teeth whitening"
-                aria-describedby="email-optional"
+                type="text"
+                name="subject"
+                id="subject"
+                className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Subject of your message"
+                aria-describedby="subject"
+              />
+            </div>
+            <div>
+              <div className="flex gap-1">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Message
+                </label>
+                <span
+                  className="text-sm leading-6 text-red-500"
+                  id="description"
+                >
+                  *
+                </span>
+              </div>
+              <div className="mt-2">
+                <textarea
+                  type="message"
+                  name="message"
+                  id="message"
+                  className="block w-full h-40 rounded-md border-0 py-1.5 text-gray-700 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-5"
+                  placeholder="Please write your message here. 400 characters max."
+                  aria-describedby="message"
+                  maxLength={400}
+                ></textarea>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 mt-5 md:mt-0">
+            <div>
+              <div className="flex gap-1">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Referer's full name
+                </label>
+              </div>
+              <input
+                type="text"
+                name="refer"
+                id="refer"
+                className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Name of the referer"
+                aria-describedby="refer"
+              />
+            </div>
+            <div className="mt-5 ">
+              <div className="flex gap-1">
+                <label
+                  htmlFor="referer"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Referer's email address
+                </label>
+              </div>
+              <input
+                type="text"
+                name="referer"
+                id="referer"
+                className="block w-full mt-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Email of the referer"
+                aria-describedby="referer"
               />
             </div>
           </div>
         </div>
 
         <div className=" md:flex  justify-end">
-          <button className="text-white w-full md:w-24  bg-indigo-500 px-2 h-8 mt-5 rounded-sm hover:bg-indigo-700 cursor-pointer">
+          <button className="text-white w-full md:w-28  bg-indigo-500 px-2 h-8 mt-5 rounded-sm hover:bg-indigo-700 cursor-pointer">
             Save
           </button>
         </div>
