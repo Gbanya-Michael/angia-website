5294fb24beb8b5ec32760a0d25129ae8109412f5
Pennymike <gbanyamichael1@gmail.com>
2023-12-28 09:06:57 +1100

    added data file

---
+++ src/data/useData.js
@@ -0,0 +1,32 @@
+// data for services
+export const services = [
+  {
+    id: "1",
+    title: "Web Development",
+    description: `Elevate your online presence with our custom web development
+                    services. We use modern technologies to modify or introduce your online
+                    presence in style.`,
+  },
+  {
+    id: "2",
+    title: "SEO Services",
+    description: `Dominate search engines with our 
+    powerful SEO optimization services. We make sure you are being found online
+    by powerful search engines like google.
+                    `,
+  },
+  {
+    id: "3",
+    title: "SSM Services",
+    description: `Low or zero social media presence? We will set up standard social media accounts
+     and connect to your webiste. This will 
+     boost traffic and increase sales.`,
+  },
+  {
+    id: "4",
+    title: "SSM Services",
+    description: `Low or zero social media presence? We will set up standard social media accounts
+     and connect to your webiste. This will 
+     boost traffic and increase sales.`,
+  },
+];
