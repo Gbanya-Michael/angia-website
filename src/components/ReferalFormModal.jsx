8b97f8203af0e618e4a576911b471812a1922acc
Pennymike <gbanyamichael1@gmail.com>
2023-12-27 21:00:19 +1100

    chanaged bg color

--- src/components/ReferalFormModal.jsx
+++ src/components/ReferalFormModal.jsx
@@ -10,9 +10,9 @@ export default function ReferalFormModal() {
   return (
     <>
       {isReferalFormOpen ? (
-        <div className="pt-28  bg-black/80  w-screen absolute  z-50 bottom-0 top-0 left-0 right-0">
+        <div className="pt-28  bg-black/80 dark:bg-white/60 w-screen absolute  z-50 bottom-0 top-0 left-0 right-0">
           <Container>
-            <div className=" p-3 absolute md:p-5 right-0 left-0 rounded-sm bg-white dark:bg-black w-[85%] mx-auto max-w-7xl ">
+            <div className=" p-3 absolute md:p-5 right-0 left-0 rounded-sm bg-white/90 dark:bg-black w-[85%] mx-auto max-w-7xl ">
               <button
                 onClick={closeReferalForm}
                 className="ml-auto mb-5 ring-1 ring-black dark:ring-white rounded-sm text-black dark:text-white flex justify-end flex-shrink-0 hover:bg-black/50 dark:hover:bg-white hover:text-white dark:hover:text-black"
@@ -20,7 +20,7 @@ export default function ReferalFormModal() {
                 <XMarkIcon className="  w-6 h-6" />
               </button>
               <div className="text-gray-800 dark:text-white text-center my-5 text-sm md:text-lg">
-                Contact Form
+                Referral Form
               </div>
               <div className="text-gray-600 dark:text-white/80 my-5 text-sm md:text-lg">
                 <p className="my-5">
