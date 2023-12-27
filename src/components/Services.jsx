2a45a87d43e5b175d0d098d7bba63817c701175a
Pennymike <gbanyamichael1@gmail.com>
2023-12-28 09:10:49 +1100

    added services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,37 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import NavBar from "./nav";
 import Container from "./Container";
 import Footer from "./Footer";
 import ContactFormModal from "../modals/ContactFormModal";
 import ReferalFormModal from "../modals/ReferalFormModal";
 import { useForm } from "../contexts/FormContext";
-
+import { services } from "../data/useData";
 //
-const services = [
-  {
-    id: "1",
-    title: "Web Development",
-    description: `Elevate your online presence with our custom web development
-                    services. We use modern technologies to modify or introduce your online
-                    presence in style.`,
-  },
-  {
-    id: "2",
-    title: "SEO Services",
-    description: `Dominate search engines with our 
-    powerful SEO optimization services. We make sure you are being found online
-    by powerful search engines like google.
-                    `,
-  },
-  {
-    id: "3",
-    title: "SSM Services",
-    description: `Low or zero social media presence? We will set up standard social media accounts
-     and connect to your webiste. This will 
-     boost traffic and increase sales.`,
-  },
-];
 
 //
 
