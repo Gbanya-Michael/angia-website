d166b1657882f02ea055d79546505a298bd417d8
Pennymike <gbanyamichael1@gmail.com>
2024-01-01 19:29:02 +1100

    fixed margin

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -68,7 +68,7 @@ export default function ContactForm() {
               type="tel"
               name="phone-number"
               id="phone-number"
-              className="block w-full bg-white/80  rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full mt-2 bg-white/80  rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Ex. 090 6333 6333"
               aria-describedby="phone-number"
             />
