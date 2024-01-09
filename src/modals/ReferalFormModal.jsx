7ab531e09dda339945273b4bebd61a409f11efdb
Pennymike <gbanyamichael1@gmail.com>
2024-01-09 18:48:51 +1100

    styles

--- src/modals/ReferalFormModal.jsx
+++ src/modals/ReferalFormModal.jsx
@@ -9,9 +9,9 @@ export default function ReferalFormModal() {
   return (
     <>
       {isReferalFormOpen ? (
-        <div className=" pt-52  bg-black/80 dark:bg-white/60 w-screen absolute  z-50 bottom-0 top-0 left-0 right-0">
+        <div className=" bg-black/90 dark:bg-white/60 w-screen absolute  z-50 bottom-0 top-0 left-0 right-0">
           <Container>
-            <div className=" p-3 absolute md:p-5 right-0 left-0 rounded-sm bg-white/90 dark:bg-black w-[85%] mx-auto max-w-7xl ">
+            <div className="h-[32rem] overflow-y-scroll  p-3 fixed top-20 md:top-30 md:p-5 right-0 left-0 rounded-sm bg-white/90 dark:bg-black w-[85%] mx-auto max-w-7xl ">
               <button
                 onClick={closeReferalForm}
                 className="ml-auto mb-5 ring-1 ring-black dark:ring-white rounded-sm text-black dark:text-white flex justify-end flex-shrink-0 hover:bg-black/50 dark:hover:bg-white hover:text-white dark:hover:text-black"
