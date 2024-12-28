2767fd570345a1905894d6caa517ec77610b84c0
Pennymike <gbanyamichael1@gmail.com>
2024-12-29 06:49:54 +1100

    update home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "../components/Home";
+import Sidebar from "../components/nav";
 
 export default function HomePage() {
-  return <Home />;
+  return <Sidebar content={<Home />} />;
 }
