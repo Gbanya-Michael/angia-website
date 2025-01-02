f91297cfaf234b7ceab5f505aab733dd4023b6c8
Pennymike <gbanyamichael1@gmail.com>
2025-01-02 15:11:23 +1100

    Overhaul everything 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ServicesPage from "./pages/ServicesPage";
@@ -11,52 +10,18 @@ import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
-    <Router>
-      <div className="dark:bg-black bg-gray-200 h-screen">
-        <ScrollToTop />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Helmet>
-                  <title>
-                    Angia Technologies | Custom Software Development Sydney
-                  </title>
-                  <meta
-                    name="description"
-                    content="Leading software development company in Sydney. Specializing in custom software solutions and digital products."
-                  />
-                  <link rel="canonical" href="https://angia.com.au" />
-                </Helmet>
-                <HomePage />
-              </>
-            }
-          />
-          <Route
-            path="/about"
-            element={
-              <>
-                <Helmet>
-                  <title>About Us | Angia Technologies</title>
-                  <meta
-                    name="description"
-                    content="Learn about Angia Technologies, a leading software development company in Sydney delivering innovative digital solutions."
-                  />
-                  <link rel="canonical" href="https://angia.com.au/about" />
-                </Helmet>
-                <AboutPage />
-              </>
-            }
-          />
-          <Route path="/services" element={<ServicesPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/products/:productId" element={<ProductPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:slug" element={<BlogPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="dark:bg-black bg-gray-200 h-screen">
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/products/:productId" element={<ProductPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:slug" element={<BlogPage />} />
+      </Routes>
+    </div>
   );
 }
 
